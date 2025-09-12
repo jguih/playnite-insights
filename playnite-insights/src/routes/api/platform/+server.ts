27c80f51e1e4cab511622d62c4c295d461bc31e5
Jose Guilherme <gui.kcoal+github@hotmail.com>
2025-09-11 23:29:26 -0300

    chore: add caching for platforms api route

--- playnite-insights/src/routes/api/platform/+server.ts
+++ playnite-insights/src/routes/api/platform/+server.ts
@@ -1,23 +1,25 @@
 import { services } from '$lib';
+import { handleApiError } from '$lib/server/api/handle-error';
+import { createHashForObject } from '$lib/server/api/hash';
 import { emptyResponse, getAllPlatformsResponseSchema } from '@playnite-insights/lib/client';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
-export const GET: RequestHandler = () => {
-	const _data = services.platformRepository.all();
+export const GET: RequestHandler = ({ request }) => {
+	const ifNoneMatch = request.headers.get('if-none-match');
 
-	if (!_data || _data.length === 0) {
-		return emptyResponse();
+	try {
+		const data = services.platformRepository.all();
+		if (!data || data.length === 0) {
+			return emptyResponse();
+		}
+		getAllPlatformsResponseSchema.parse(data);
+		const hash = createHashForObject(data);
+		const etag = `"${hash}"`;
+		if (ifNoneMatch === etag) {
+			return emptyResponse(304);
+		}
+		return json(data, { headers: { 'Cache-Control': 'no-cache', ETag: etag } });
+	} catch (err) {
+		return handleApiError(err);
 	}
-
-	const result = getAllPlatformsResponseSchema.safeParse(_data);
-
-	if (!result.success) {
-		services.log.error(`Schema validation failed: ${result.error.format()}`);
-		return json(
-			{ error: { message: 'Invalid server data', details: result.error.flatten() } },
-			{ status: 500 },
-		);
-	}
-
-	return json(result.data);
 };
