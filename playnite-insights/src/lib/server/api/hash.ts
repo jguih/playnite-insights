e1c29245063763af61286720a79a4ba71d29cbcb
Jose Guilherme <gui.kcoal+github@hotmail.com>
2025-09-12 09:46:28 -0300

    chore: impove etag generation

--- playnite-insights/src/lib/server/api/hash.ts
+++ playnite-insights/src/lib/server/api/hash.ts
@@ -1,6 +1,23 @@
 import { createHash } from 'crypto';
 
+const sortKeys = (value: unknown): unknown => {
+	if (Array.isArray(value)) {
+		return value.map(sortKeys);
+	} else if (value && typeof value === 'object') {
+		return Object.keys(value)
+			.sort()
+			.reduce(
+				(acc, key) => {
+					(acc as Record<string, unknown>)[key] = sortKeys((value as Record<string, unknown>)[key]);
+					return acc;
+				},
+				{} as Record<string, unknown>,
+			);
+	}
+	return value;
+};
+
 export const createHashForObject = (data: unknown) => {
-	const jsonStr = JSON.stringify(data);
-	return createHash('sha256').update(jsonStr).digest('hex');
+	const jsonStr = JSON.stringify(sortKeys(data));
+	return createHash('sha1').update(jsonStr).digest('hex');
 };
