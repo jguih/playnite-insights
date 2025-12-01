736ac4c16ce841b89a967fe3d01f5c617836f6a2
Jose Guilherme <gui.kcoal+github@hotmail.com>
2025-12-01 10:02:47 -0300

    chore: update api games endpoint to use new PlayAtlas api

--- playnite-insights/src/routes/api/game/+server.ts
+++ playnite-insights/src/routes/api/game/+server.ts
@@ -1,20 +1,11 @@
 import { withInstanceAuth } from '$lib/server/api/authentication';
-import { createHashForObject } from '$lib/server/api/hash';
-import { emptyResponse, getAllGamesResponseSchema } from '@playnite-insights/lib/client';
+import { emptyResponse } from '@playnite-insights/lib/client';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
-export const GET: RequestHandler = ({ request, url, locals: { services } }) =>
+export const GET: RequestHandler = ({ request, url, locals: { services, api } }) =>
 	withInstanceAuth(request, url, services, async () => {
 		const ifNoneMatch = request.headers.get('if-none-match');
-		const data = services.playniteGameRepository.all();
-		if (!data || data.length === 0) {
-			return emptyResponse();
-		}
-		getAllGamesResponseSchema.parse(data);
-		const hash = createHashForObject(data);
-		const etag = `"${hash}"`;
-		if (ifNoneMatch === etag) {
-			return emptyResponse(304);
-		}
-		return json(data, { headers: { 'Cache-Control': 'no-cache', ETag: etag } });
+		const result = api.gameLibrary.queries.getAllGamesHandler().execute({ ifNoneMatch });
+		if (result.type === 'not_modified') return emptyResponse(304);
+		else return json(result.data, { headers: { 'Cache-Control': 'no-cache', ETag: result.etag } });
 	});
